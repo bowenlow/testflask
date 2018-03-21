2acca92495a32dcbb8c3ddf8d79cf359d9794798
Bowen <bowenlow88@git.generalassemb.ly>
2018-03-22 01:30:03 +0800

    first
    first

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+# from app import routes
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return 'Hello, World!'
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
